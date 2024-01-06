3b9921684a0ee8c3743ea94cebf7eed7e7527232
Alessandro Semeraro <asemerar@f1r1s4.codam.nl>
2024-01-06 17:30:38 +0100

    added_funciton of rotate and reverse rotate

--- mov_rev_rotate.c
+++ mov_rev_rotate.c
@@ -1,44 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   mov_swap.c                                         :+:    :+:            */
+/*   mov_rev_rotate.c                                   :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: asemerar <asemerar@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2024/01/06 14:17:30 by asemerar      #+#    #+#                 */
-/*   Updated: 2024/01/06 14:17:31 by asemerar      ########   odam.nl         */
+/*   Created: 2024/01/06 14:17:35 by asemerar      #+#    #+#                 */
+/*   Updated: 2024/01/06 17:29:05 by asemerar      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
 #include "push_swap.h"
 
-static void	ft_swap(t_list	**stack)
+static void	ft_reverse_rotate(t_list **stack)
 {
 	t_list	*tp;
-
-	if((*stack == NULL || (*stack)->next == NULL))
+	t_list	*last_node;
+	
+	if(!((*stack) || (*stack)->next))
 		return ;
 	tp = *stack;
-	*stack = (*stack)->next;
-	tp->next = (*stack)->next;
-	(*stack)->next = tp;
+	last_node = ft_lstlast(*stack);
+	while((*stack)->next->next)
+	{
+		*stack = (*stack)->next;
+	}
+	(*stack)->next = NULL;
+	last_node->next = tp;
+	*stack = last_node;
 }
 
-void	sa(t_list **stack_a)
+void	rra(t_list **stack_a)
 {
-	ft_swap(stack_a);
-	write(1, "sa\n", 3);
+	ft_reverse_rotate(stack_a);
+	write(1, "rra\n", 4);
 }
-void	sb(t_list	**stack_b)
+
+void	rra(t_list **stack_b)
 {
-	ft_swap(stack_b);
-	write (1, "sb\n", 3);
+	ft_reverse_rotate(stack_b);
+	write(1, "rrb\n", 4);
 }
-void	ss(t_list **stack_a, t_list **stack_b)
-{
-	ft_swap(stack_a);
-	ft_swap(stack_b);
-	write (1, "ss\n", 3);
-}
+
---
+++ mov_rotate.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   move_rotate.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: asemerar <asemerar@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/01/06 14:17:19 by asemerar      #+#    #+#                 */
+/*   Updated: 2024/01/06 16:17:11 by asemerar      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	ft_rotate(t_list **stack)
+{
+	t_list	*first_node;
+
+	first_node = *stack;
+	*stack = first_node->next;
+	first_node->next = NULL;
+	ft_lstlast(*stack)->next = first_node;
+}
+
+void	ra(t_list **stack_a)
+{
+	ft_rotate(stack_a);
+	write(1, "ra\n", 3);
+}
+
+void	rb(t_list **stack_b)
+{
+	ft_rotate(stack_b);
+	write(1, "rb\n", 3);
+}
+
+// static void	ft_rotate(t_list **stack)
+// {
+// 	t_list	*tp;
+// 	t_list	*head;
+// 	t_list	*soc;
+
+// 	tp = *stack;
+// 	head = *stack;
+// 	while(tp && tp->next)
+// 	{
+// 		tp = tp->next;
+// 	}
+// 	soc = tp;
+// 	tp->next = head->next;
+// 	tp = head;
+// 	head = soc;
+// }
