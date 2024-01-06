b2f126c24b2ec626403100706e0c20cb27861423
Alessandro Semeraro <asemerar@f0r1s2.codam.nl>
2024-01-06 23:02:58 +0100

    added push functions

---
+++ move_pushes.c
@@ -0,0 +1,32 @@
+#include "push_swap.h"
+
+void	pa(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*tp_a;
+	t_list	*tp_b;
+
+	if (!*stack_b)
+		return ;
+
+	tp_a = *stack_a;
+	tp_b = *stack_b;
+	*stack_b = tp_b->next;
+	tp_b->next = tp_a;
+	*stack_a = tp_b;
+	write(1, "pa\n", 3);
+}
+
+void	pb(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*tp_a;
+	t_list	*tp_b;
+
+	if(!*stack_a)
+		return ;
+	tp_a = *stack_a;
+	tp_b = *stack_b;
+	*stack_a = tp_a->next;
+	tp_a->next = tp_b;
+	*stack_b = tp_a;
+	write(1, "pb\n", 3);
+}
