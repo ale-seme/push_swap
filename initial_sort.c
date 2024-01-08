0962fc3e62f5bde57fe5ae84961320d8530f129c
Alessandro Semeraro <alexei.semeraro@gmail.com>
2024-01-08 11:58:42 +0100

    almost done with sort 3 numbers case

--- initial_sort.c
+++ initial_sort.c
@@ -7,12 +7,33 @@ void	node3_sort(t_swap *tab)
 	if(is_sorted(&tab->stack_a))
 		return ;
 	last_in_stack = ft_lstlast(tab->stack_a);
-
+	if (is_sorted_reversed(&tab->stack_a))
+	{
+		sa(&tab->stack_a);
+		rra(&tab->stack_a);
+	}
+	else_if(tab->stack_a->content > tab->stack_a->next->content
+		&& tab->stack_a->next->content < last_in_stack->content)
+	{
+		ra(&tab->stack_a);
+	}
 	else if(tab->stack_a->content > tab->stack_a->next->content
 		&& tab->stack_a->next->content < last_in_stack->content)
-		{
+	{
+		sa(&tab->stack_a);
+	}
+	else if(tab->stack_a->content < tab->stack_a->next->content
+		&& tab->stack->next->content > last_in_stack->conten)
+	{
+		rra(&tab->stack_a);
+	}
+	else if(tab->stack_a->content < tab->stack_a->next->content
+		&& tab->stack_a->next->content > last_in_stack->content)
+	{
+		rra(&tab->stack_a);
+		sa(&tab->stack_a);
+	}
 
-		}
 }
 
 
