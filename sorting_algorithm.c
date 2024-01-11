20a0a2dcd3cb29143114bab121c28f6051f376ec
Alessandro Semeraro <alexei.semeraro@gmail.com>
2024-01-11 22:35:52 +0100

    starting to write the sorting algorithm

---
+++ sorting_algorithm.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   sorting_algorithm.c                                :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: asemerar <asemerar@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/01/11 22:31:07 by asemerar      #+#    #+#                 */
+/*   Updated: 2024/01/11 22:35:21 by asemerar      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void divide_in_two(t_list **stack_a, t_list **stack_b, int len)
+{
+    
+}
