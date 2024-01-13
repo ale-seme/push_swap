5004d871347493cbd5a3d8d632a8c2b12cd01d44
Alessandro Semeraro <alexei.semeraro@gmail.com>
2024-01-13 23:25:56 +0100

    added function that shlould start emptying stack_b

--- sorting_algorithm.c
+++ sorting_algorithm.c
@@ -6,7 +6,7 @@
 /*   By: asemerar <asemerar@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/11 22:31:07 by asemerar      #+#    #+#                 */
-/*   Updated: 2024/01/13 20:14:47 by asemerar      ########   odam.nl         */
+/*   Updated: 2024/01/13 23:25:18 by asemerar      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,34 @@ void divide_in_two(t_list **stack_a, t_list **stack_b, t_push *push, int origin_
 	push->mid = (push->max - push->next) / 2 + push->next;
 	push->flag++;
 }
+void	sorting_emptying_b(t_list **stack_a, t_list **stack_b, t_push *push)
+{
+	int	len_b;
+	int	i;
 
+	i = 0;
+	len_b = ft_lstsize(*stack_b);
+	while(i < len_b && ft_lstsize(*stack_b))
+	{
+		if((*stack_b)->index == push->next)
+		{
+			find_next(stack_a, stack_b, push);
+		}
+		else if ((*stack_b)->index >= push->mid)
+		{
+			(*stack_b)->flag = push->flag;
+			pa(stack_a, stack_b);
+		}
+		else if ((ft_lstsize(*stack_b) > 1 && (*stack_b)->index < push->mid))
+		{
+			rb(stack_b);
+		}
+		i++;
+	}
+	push->max = push->mid;
+	push->mid = (push->max - push->next) / 2 + push->next;
+	push->flag++;
+}
 
 
 
@@ -50,13 +77,13 @@ void sorting_algorithm(t_list **stack_a, t_list **stack_b, int origin_len)
 	divide_in_two(stack_a, stack_b, &push, origin_len);
 	while(!is_sorted(stack_a, origin_len))
 	{
-		if(!ft_lstsize(stack_b))
+		if(ft_lstsize(stack_b) == 0)
 		{
-			sorting_1(stack_a, stack_b, &push);
+			sorting_a(stack_a, stack_b, &push);
 		}
 		else
 		{
-			sorting_2(stack_a, stack_b, &push);
+			sorting_emptying_b(stack_a, stack_b, &push);
 		}
 	}
 	
