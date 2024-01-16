7a47ccdc540e4815be26846f0f04fb5a004e6737
Alessandro Semeraro <alexei.semeraro@gmail.com>
2024-01-16 15:21:36 +0100

    added other funciton to h file

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: asemerar <asemerar@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/12/08 12:39:03 by asemerar      #+#    #+#                 */
-/*   Updated: 2024/01/16 15:19:25 by asemerar      ########   odam.nl         */
+/*   Updated: 2024/01/16 15:21:18 by asemerar      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,11 @@ void	divide_in_two(t_list **stack_a, t_list **stack_b, t_push *push, int origin_
 void	pa(t_list **stack_a, t_list **stack_b);
 void	pb(t_list **stack_a, t_list **stack_b);
 void	ra(t_list **stack_a);
+void	rb(t_list **stack_b);
+void	rra(t_list **stack_a);
+void	rra(t_list **stack_b);
+
+
 
 
 
